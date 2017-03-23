ff3a2389e8e23d24b775d9795a591fe9b2e11225
God Tariq <t@ramesar.net>
2017-03-22 22:43:23 -0400

    Initial commit. Template should be good.

---
+++ makefile
@@ -0,0 +1,12 @@
+all:
+	g++ template.cpp -o main
+a:
+	g++ a.cpp -o main
+b:
+	g++ b.cpp -o main
+c:
+	g++ c.cpp -o main
+d:
+	g++ d.cpp -o main
+clean:
+	rm main
---
+++ template.cpp
@@ -0,0 +1,40 @@
+// OB3LISK's C++ template for Google Code Jam.
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+// Handlf reading and writing to file.
+freopen("A.in", "r", stdin); // redirects standard input
+freopen("A.out", "w", stdout); // redirects standard output
+
+// Do all the work for the code jam problem here.
+// Write the answer to a test case.
+void solve()
+    {
+    
+    }
+
+// Program begins here. Handles writing "Case #: "
+int main()
+    {
+    int tt; cin >> tt;
+    for(int t = 1; t <= tt; t++)
+        {
+        cout << "Case #" << t << ": ";
+        solve();
+        if(t != tt) { cout << "\n"; }
+        }
+    return 0;
+    }
